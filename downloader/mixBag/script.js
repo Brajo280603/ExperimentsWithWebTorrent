b41164a419e9f184137a9c288b32aa4b356e35ec
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-03-14 11:59:46 +0530

    first test

---
+++ downloader/mixBag/script.js
@@ -0,0 +1,13 @@
+
+import WebTorrent from "webtorrent";
+const client = new WebTorrent()
+const magnetURI = 'magnet:?xt=urn:btih:C3C49D28FF03C63AB115D0AEB06326E7495F9AC7&dn=Morbius+2022+1080p+WEBRip+AAC+x265-BluBeast&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce';
+
+client.add(magnetURI, torrent => {
+  // Got torrent metadata!
+  console.log('Client is downloading:', torrent.infoHash)
+
+  for (const file of torrent.files) {
+    document.body.append(file.name)
+  }
+})
